81099d788658b83a46534067cd53011d02edf256
Jonathan Rochkind <jonathan@dnil.net>
2013-06-23 22:08:28 -0400

    config.threadsafe

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module FakeWorkApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.threadsafe!  # threadsafe!
   end
 end
